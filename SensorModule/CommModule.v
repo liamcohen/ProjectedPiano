f27aec077f8acb5c40c1a8f34f61729ce60e13e7
Liam A Cohen <lc2017@mit.edu>
2017-11-06 15:06:32 -0500

    comm module skeleton

---
+++ SensorModule/CommModule.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:05:52 11/06/2017 
+// Design Name: 
+// Module Name:    CommModule 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module CommModule(
+    input request_data,
+    output data_ready,
+    inout sda,
+    output scl,
+    output [7:0] data_out,
+    input meas_ready
+    );
+
+
+endmodule
