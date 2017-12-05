d91224075966d69240311daaf1bbca104abfbcf6
Liam A Cohen <lc2017@mit.edu>
2017-12-05 16:17:41 -0500

    broken i2c read

---
+++ SensorModule/_impact.cmd
@@ -0,0 +1,32 @@
+loadProjectFile -file "/afs/athena.mit.edu/user/l/c/lc2017/Desktop/6.111_Final_Project/ProjectedPiano/SensorModule/SensorModule.ipf"
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+addDevice -p 2 -file "/afs/athena.mit.edu/user/l/c/lc2017/Desktop/6.111_Final_Project/ProjectedPiano/SensorModule/labkit.bit"
+assignFile -p 2 -file "/afs/athena.mit.edu/user/l/c/lc2017/Desktop/6.111_Final_Project/ProjectedPiano/SensorModule/labkit.bit"
+Program -p 2 
+Program -p 2 
+Program -p 2 
+Program -p 2 
+Program -p 2 
+saveProjectFile -file "/afs/athena.mit.edu/user/l/c/lc2017/Desktop/6.111_Final_Project/ProjectedPiano/SensorModule/SensorModule.ipf"
+setMode -bs
+deleteDevice -position 1
+deleteDevice -position 1
+deleteDevice -position 1
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
