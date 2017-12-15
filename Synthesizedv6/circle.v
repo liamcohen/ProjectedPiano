4c190dd1759ecc0277bc6f4f5d4d3606ed0c7c6a
liamcohen <lc2017@mit.edu>
2017-12-15 16:27:09 -0500

    Update circle.v

--- Synthesizedv6/circle.v
+++ Synthesizedv6/circle.v
@@ -22,18 +22,16 @@ module circle
    #(parameter RADIUS = 35)            // default radius: 35 pixels
    (input [10:0] x,hcount,
     input [9:0] y,vcount,
-	 input [23:0] color,
+    input [23:0] color,
     output reg [23:0] pixel);
 
-   reg [10:0] x_length;
-	reg [9:0] y_length;
+    reg [10:0] x_length;
+    reg [9:0] y_length;
 	
-	always @ * begin
-      x_length <= hcount - x;
-		y_length <= vcount - y;
-		
-		if ((((x_length - RADIUS) * (x_length - RADIUS)) + ((y_length - RADIUS) * (y_length - RADIUS))) <= (RADIUS * RADIUS)) pixel <= color;
-		else pixel <= 0;
-		
-   end
+    always @ * begin
+       x_length <= hcount - x;
+       y_length <= vcount - y;	
+       if ((((x_length - RADIUS) * (x_length - RADIUS)) + ((y_length - RADIUS) * (y_length - RADIUS))) <= (RADIUS * RADIUS)) pixel <= color;
+       else pixel <= 0;	
+    end
 endmodule
