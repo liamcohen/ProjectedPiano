a3e681225f5952196bbba918c92ef7dfe3677652
liamcohen <lc2017@mit.edu>
2017-12-15 16:54:47 -0500

    Update play_instrument.v

--- Synthesizedv6/play_instrument.v
+++ Synthesizedv6/play_instrument.v
@@ -18,52 +18,52 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module play_instrument#(RECORDING_LEN = 32768)
+module play_instrument#(RECORDING_LEN = 16384)
 
 (
    input wire clock,	           // 27mhz system clock
    input wire reset,                // 1 to reset to initial state
    input wire playback,             // 1 for playback, 0 for record
    input wire ready,                // 1 when AC97 data is available
-	input wire [18:0] address_in,
+   input wire [18:0] address_in,
    input wire [7:0] from_ac97_data, // 8-bit PCM data from mic
    output reg [7:0] to_ac97_data,  // 8-bit PCM data to headphone
    output reg we_ZBT,
-	input wire [35:0] data_in, //audio data for writing to ZBT
-	output reg [35:0] data_out, //audio data from reading ZBT
-	output reg [18:0] address,   //ZBT address
-	output reg [7:0] led);
+   input wire [35:0] data_in, //audio data for writing to ZBT
+   output reg [35:0] data_out, //audio data from reading ZBT
+   output reg [18:0] address,   //ZBT address
+   output reg [7:0] led);
 	 
-	reg started = 0;
-	//wire [19:0] tone;
+   reg started = 0;
+
    reg [1:0] counter = 0;
-   //reg send = 0;
+
    
-	reg [LOGSIZE-1:0] highest_address;
+   reg [LOGSIZE-1:0] highest_address;
 
    always @ (posedge clock) begin
       //led<= 8'b11111111;
       mode <= playback;
-		if (~started) begin
-		    address <= start_address;
-			 counter <= 0;
-	   end
-	   we_ZBT <= 1; //don't write to memory just read it
-	   //led[1:0]<= ~2'b10;
-	    if (ready)begin
-	       counter <= counter + 1;
-	       if (address == address_in + RECORDING_LEN) begin
-	          counter <= 0;
-	          address <= address_in;
-	       end
-
-		    else if (counter == 3) begin 
-			    //if (counter == 1) begin
-	          address <= address + 1;
-			    to_ac97_data <= data_in;
-		    end
-		    data_out <= 36'hZ;
+         if (~started) begin
+	    address <= start_address;
+	    counter <= 0;
+	 end
+	 we_ZBT <= 1; //don't write to memory just read it
+	 //led[1:0]<= ~2'b10;
+	 if (ready)begin
+	    counter <= counter + 1;
+	    if (address == address_in + RECORDING_LEN) begin
+	       counter <= 0;
+	       address <= address_in;
 	    end
+
+	    else if (counter == 3) begin 
+			   
+	       address <= address + 1;
+	       to_ac97_data <= data_in;
+	    end
+	       data_out <= 36'hZ;
+	end
    end
 
 
